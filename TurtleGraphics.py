fac620c1249286dcce8aa38025e7957876c7d9d0
Derek Babb <dbabb1@gmail.com>
2025-02-17 09:11:22 -0600

    Update TurtleGraphics.py

--- TurtleGraphics.py
+++ TurtleGraphics.py
@@ -12,13 +12,40 @@ def drawSquare(myTurtle, size):
         myTurtle.right(90)
 
 
+def drawPolygon(myTurtle, sides):
+    angle = 360 / sides
+    for s in range(sides):
+        myTurtle.forward(50)
+        myTurtle.right(angle)
+
+
+def fillCorner(myTurtle, corner):
+    myTurtle.up()
+    myTurtle.goto(-100, 100)
+    myTurtle.down()
+    
+    #big square
+    drawSquare(myTurtle, 200)
+    
+    #move to position
+    if corner == 2:
+        myTurtle.forward(100)
+    if corner == 3:
+        myTurtle.right(90)
+        myTurtle.forward(100)
+        myTurtle.left(90)
+    
+    myTurtle.begin_fill()
+    drawSquare(myTurtle, 100)
+    myTurtle.end_fill()
+
 def main():
     myTurtle = turtle.Turtle()
-    # drawPolygon(myTurtle, 5) #draws a pentagon
-    # drawPolygon(myTurtle, 8) #draws an octogon
+    #drawPolygon(myTurtle, 5) #draws a pentagon
+    #drawPolygon(myTurtle, 8) #draws an octogon
 
-    # fillCorner(myTurtle, 2) #draws a square with top right corner filled in.
-    # fillCorner(myTurtle, 3) #draws a square bottom left corner filled in.
+    #fillCorner(myTurtle, 2) #draws a square with top right corner filled in.
+    fillCorner(myTurtle, 3) #draws a square bottom left corner filled in.
 
     # squaresInSquares(myTurtle, 5) #draws 5 concentric squares
     # squaresInSquares(myTurtle, 3) #draws 3 concentric squares
